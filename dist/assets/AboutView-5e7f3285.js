c41747bc96768e92cdb1c1482c8233eec2c5cad1
Lilibeth <bethfabregascpa@gmail.com>
2023-02-15 03:06:54 +0800

    commit new build

---
+++ dist/assets/AboutView-5e7f3285.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as s,a as o}from"./index-7728cbed.js";const c={},a={class:"bg-green-500"},n=o("h1",{className:"text-3xl font-bold underline"},"This is the about page",-1),_=[n];function r(i,d){return t(),s("div",a,_)}const f=e(c,[["render",r]]);export{f as default};
